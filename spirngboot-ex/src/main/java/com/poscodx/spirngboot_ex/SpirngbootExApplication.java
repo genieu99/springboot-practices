db60e3c9417a215b10f9468f8d0c944a4eb1edc2
genieu99 <yujin0419@sookmyung.ac.kr>
2024-06-24 15:25:31 +0900

    class: springboot-ex

---
+++ spirngboot-ex/src/main/java/com/poscodx/spirngboot_ex/SpirngbootExApplication.java
@@ -0,0 +1,13 @@
+package com.poscodx.spirngboot_ex;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpirngbootExApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpirngbootExApplication.class, args);
+	}
+
+}
---
+++ spirngboot-ex/src/test/java/com/poscodx/spirngboot_ex/SpirngbootExApplicationTests.java
@@ -0,0 +1,13 @@
+package com.poscodx.spirngboot_ex;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpirngbootExApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

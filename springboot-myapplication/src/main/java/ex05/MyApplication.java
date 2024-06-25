1efd6d6a1df625a30c590078ab5b0aa504374080
genieu99 <yujin0419@sookmyung.ac.kr>
2024-06-25 11:08:44 +0900

    class: @SpringBootApplication #finish

---
+++ springboot-myapplication/src/main/java/ex05/MyApplication.java
@@ -0,0 +1,29 @@
+package ex05;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.SpringBootConfiguration;
+import org.springframework.context.ApplicationContext;
+
+import ex05.component.MyComponent;
+
+/**
+ * @SpringBootApplication 메타 어노테이션
+ * 
+ * + @SpringBootConfiguration: ex) ex01, ex02
+ * + @ComponentsScan: ex) 패키지 ex04를 포함, 하부 패키지를 스캔한다. ex) ex03
+ * + @EnableAutoConfiguration:
+ *    1. MVC, AOP, Security, JPA 등을 자동으로 설정
+ *    2. 발견된 Starter Dependency(Library)기반으로 설정(관례를 따르는 default 설정)
+ *    3. application.properties or application.yml 안의 미세 설정 (필수)
+ *    4. Spring Boot의 Auto Configuration이나 미세 설정 이외의 설정은 Java 설정을 한다. 
+ * 
+ */
+@SpringBootConfiguration
+public class MyApplication {
+
+	public static void main(String[] args) {
+		ApplicationContext ac = SpringApplication.run(MyApplication.class, args);
+//		MyComponent myComponent = ac.getBean(MyComponent.class);
+//		myComponent.printHello();
+	}
+}
